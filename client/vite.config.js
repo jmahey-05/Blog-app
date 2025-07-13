4dbebf54feab07b2257ae5238f52123a07202ef3
Jatin Mahey <maheyjatin2711@gmail.com>
2025-07-14 00:08:32 +0530

    client

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+const baseURL = import.meta.env.VITE_API_BASE_URL;
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server:{
     proxy:{
       '/api':{
-        target: 'http://localhost:3000',
+        target: baseURL,
         secure: false,
       },
     },
