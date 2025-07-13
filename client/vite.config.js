266f92de86960dfef18e323a6b7682281737116e
Jatin Mahey <maheyjatin2711@gmail.com>
2025-07-14 00:38:01 +0530

    client

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-const baseURL = import.meta.env.VITE_API_BASE_URL;
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  server:{
-    proxy:{
-      '/api':{
-        target: baseURL,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_BASE_URL, 
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
-  plugins: [react()],
+    plugins: [react()],
+  };
 });
