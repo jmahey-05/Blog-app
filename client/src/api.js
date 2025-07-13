a4135f7033ff7adf16347c1da6528179ee86018e
Jatin Mahey <maheyjatin2711@gmail.com>
2025-07-14 01:29:08 +0530

    client

---
+++ client/src/api.js
@@ -0,0 +1,19 @@
+// src/utils/api.js
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export const apiFetch = async (endpoint, options = {}) => {
+  const res = await fetch(`${API_BASE_URL}${endpoint}`, {
+    ...options,
+    headers: {
+      'Content-Type': 'application/json',
+      ...(options.headers || {}),
+    },
+    credentials: 'include', // if using cookies/auth
+  });
+
+  const data = await res.json();
+  if (!res.ok) {
+    throw new Error(data.message || 'API error');
+  }
+  return data;
+};
