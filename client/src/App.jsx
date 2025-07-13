4800b2cc064e01f4d5429320c79f8e2771c39617
Jatin Mahey <maheyjatin2711@gmail.com>
2025-07-13 23:15:19 +0530

    client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import './App.css'
 import Signin from './pages/Signin.jsx';
 import Signup from './pages/Signup.jsx';
 import Dashboard from './pages/Dashboard.jsx';
